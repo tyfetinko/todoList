783eb156599f0cc968f8a75943ddba684ab444c0
Ty Fetinko <ty667216@dal.ca>
2022-09-14 22:32:26 -0300

    built a basic todo list app

---
+++ list/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ToDoList from './components/ToDoList';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
